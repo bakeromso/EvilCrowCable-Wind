7f9125105ff3bd6994b0dd4217928664040ab7d0
Bakeromso <bakeromso>
2025-06-18 11:53:16 +0200

    add empty secrets file

---
+++ firmware/include/secrets.h
@@ -0,0 +1,12 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+#include <Arduino.h>
+
+// Config default SSID, password and channel
+String ssid = "";     // Enter your SSID here
+String password = ""; // Enter your Password here
+char *serverIP;
+int serverPort = 4444; // Enter the server port here
+
+#endif // SECRETS_H
